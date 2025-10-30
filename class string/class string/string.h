0094a3c708aa96389f6a8d5ad39de57d4aa1cb40
Sashkatrues <sashkastup@gmail.com>
2025-10-31 00:58:55 +0300

    new add

--- class string/class string/string.h
+++ class string/class string/string.h
@@ -2,9 +2,9 @@
 #ifndef STRING_H
 #define STRING_H
 
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
+
 class String
 {
 private:
@@ -16,10 +16,11 @@ public:
 	String(char ch);
 	String(const char* str);
 	String(const String&);
+	String(String&& other)noexcept;
 	String& operator=(const String& rhs);
 	String& operator=(const char* rhs);
 	String& operator=(char ch);
-
+	String& operator=(String&& other) noexcept;
 	String operator+(const String&) const;
 	String operator+=(const String&) const;
 	friend String operator+(const char*, const String&);
@@ -33,13 +34,20 @@ public:
 	char& operator[](size_t pos)const;
 	friend std::ostream& operator << (std::ostream& cout, const String& a);
 
+	String& copy(const String& rhs);
+	char& front();
+	char& back();
 	size_t size() const;
+	//size_t max_size() const;
+	void reserve(size_t new_len);
+	size_t capacity() const;
 	bool empty() const;
 	void clear();
 	void pop_back();
+	void swap(String& other);
 	void resize(size_t n, char ch);
 	void push_back(char ch);
-	String& append(size_t n, char ch);
+	String& append(const char* str);
 	String& append(const String&);
 	size_t find(const String&)const;
 	size_t find_first_of(const String&)const;
@@ -47,10 +55,11 @@ public:
 	String substr(size_t, size_t)const;
 	void reverse();
 	String& erase(size_t pos);
+	String& erase(size_t index, size_t count);
 	int compare(const String& rhs) const;
 	void insert(size_t pos, const String& str);
 	void replace(size_t pos, size_t count, const String& tmp);
-	char* c_str();
+	const char* c_str();
 	size_t count(char ch) const;
 	char& at(size_t pos);
 };
