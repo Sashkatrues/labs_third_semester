01b5b788686fca9882848888a0579e432d4c95e6
Sashkatrues <sashkastup@gmail.com>
2025-10-31 11:12:38 +0300

    add trains

--- stek/stek/main.cpp
+++ stek/stek/main.cpp
@@ -4,7 +4,6 @@ int main() {
     std::string input;
     std::cout << "input string: ";
     std::cin >> input;
-
     try {
         if (isBracketRight(input)) {
             std::cout << "right.\n";
@@ -17,5 +16,16 @@ int main() {
         std::cerr << "error: " << e.what();
     }
 
+    Stack stack;
+    std::string train;
+    std::cout << "input train: ";
+    std::cin >> train;
+    try {
+        std::string sortedTrain = stack.sortTrain(train);
+        std::cout << "sorted train: " << sortedTrain << '\n';
+    }
+    catch (const std::exception& e) {
+        std::cerr << "error: " << e.what();
+    }
     return 0;
 }
